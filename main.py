1bf00db86a4f58b170a03eb03214ec09e273175b
Graham Nessler <graham.nessler@gmail.com>
2025-07-11 14:55:41 -0500

    Rename SimpleTokenizerV1 to SimpleTokenizerV2, enhance tokenization with unknown token handling, and update regex for punctuation in decoding

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ vocab_size: int = len(all_tokens)
 
 base_vocab: dict[str, int] = {token: index for index, token in enumerate(all_tokens)} # The Python dictionary that maps each word to its corresponding index
 
-class SimpleTokenizerV1:
+class SimpleTokenizerV2:
     def __init__(self, base_vocab: dict[str, int]):
         self.str_to_int = base_vocab
         self.int_to_str = {i:s for s,i in base_vocab.items()}
@@ -34,18 +34,24 @@ class SimpleTokenizerV1:
         preprocessed = [
             item.strip() for item in preprocessed if item.strip()
         ]
+        preprocessed = [
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
+        ]
         ids: list[int] = [self.str_to_int[s] for s in preprocessed]
         return ids
         
     def decode(self, ids: list[int]) -> str:
         text: str = " ".join([self.int_to_str[i] for i in ids])
         # Replace spaces before the specified punctuations
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
     
-tokenizer: SimpleTokenizerV1 = SimpleTokenizerV1(base_vocab)
+tokenizer: SimpleTokenizerV2 = SimpleTokenizerV2(base_vocab)
 
-# text = "Ken Garner is a very good man."
-# ids = tokenizer.encode(text)
-# print(ids)
-# print(tokenizer.decode(ids))
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+
+text = " <|endoftext|> ".join((text1, text2))
+
+print(tokenizer.decode(tokenizer.encode(text)))
